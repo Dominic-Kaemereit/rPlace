1bdc015bd6d825b5ea87a61451ef3fe67a4fe90e
D151l <d151l@d151l.de>
2022-04-16 20:27:05 +0200

    add sql support

--- place-plugin/src/main/kotlin/de/d151l/place/plugin/database/mysql/MySQL.kt
+++ place-plugin/src/main/kotlin/de/d151l/place/plugin/database/mysql/MySQL.kt
@@ -3,6 +3,9 @@ package de.d151l.place.plugin.database.mysql
 import de.d151l.place.api.block.BlockHistory
 import de.d151l.place.api.database.DatabaseSupport
 import de.d151l.place.api.player.PlacePlayer
+import de.d151l.place.plugin.block.BlockHistoryImpl
+import de.d151l.place.plugin.player.PlacePlayerImpl
+import java.sql.*
 import java.util.*
 
 /**
@@ -12,59 +15,167 @@ import java.util.*
  */
 class MySQL: DatabaseSupport {
 
+    private lateinit var connection: Connection
+
     override fun connect(host: String, port: Int, user: String, password: String, database: String) {
-        TODO("Not yet implemented")
+        try {
+            val connectionUrl = ("jdbc:sqlserver://$host:$port;"
+                    + "database=$database;"
+                    + "user=$user@$host;"
+                    + "password=$password;"
+                    + "encrypt=true;"
+                    + "trustServerCertificate=false;"
+                    + "loginTimeout=30;")
+            this.connection = DriverManager.getConnection(connectionUrl);
+            val statement: Statement = connection.createStatement();
+            statement.executeUpdate(
+                "CREATE TABLE IF NOT EXISTS placePlayer(" +
+                        "uuid VARCHAR NOT NULL," +
+                        "name VARCHAR NOT NULL," +
+                        "blocks INTEGER NOT NULL," +
+                        "lastBlockRePlace VARCHAR NOT NULL," +
+                        "ranking INTEGER NOT NULL," +
+                        "UNIQUE(uuid) ON CONFLICT REPLACE)"
+            );
+            statement.executeUpdate(
+                "CREATE TABLE IF NOT EXISTS placeBlockHistory(" +
+                        "uuid VARCHAR NOT NULL," +
+                        "time VARCHAR NOT NULL," +
+                        "location VARCHAR NOT NULL)"
+            );
+        } catch (exception: SQLException) {
+            exception.printStackTrace();
+        }
     }
 
     override fun closeConnection() {
-        TODO("Not yet implemented")
+        try {
+            connection.close()
+        } catch (exception: SQLException) {
+            exception.printStackTrace()
+        }
     }
 
     override fun isPlayerRegistered(uuid: UUID): Boolean {
-        TODO("Not yet implemented")
+        val preparedStatement = connection
+            .prepareStatement("SELECT * FROM placePlayer WHERE uuid = ?")
+        preparedStatement.setString(1, uuid.toString())
+
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+        if (resultSet.next())
+            return true
+        return false
     }
 
     override fun getPlacePlayer(uuid: UUID): PlacePlayer {
-        TODO("Not yet implemented")
+        val preparedStatement = connection
+            .prepareStatement("SELECT * FROM placePlayer WHERE uuid = ?")
+        preparedStatement.setString(1, uuid.toString())
+
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+
+        val player: PlacePlayerImpl = PlacePlayerImpl(
+            resultSet.getString("uuid"),
+            resultSet.getString("name")
+        )
+        player.setRanking(resultSet.getInt("ranking"))
+        player.setLastBlockRePlace(resultSet.getString("lastBlockRePlace").toLong())
+        player.setBlockToCount(resultSet.getInt("blocks"))
+        return player
     }
 
     override fun getPlacePlayerByName(name: String): PlacePlayer {
-        TODO("Not yet implemented")
+        val preparedStatement = connection
+            .prepareStatement("SELECT * FROM placePlayer WHERE name = ?")
+        preparedStatement.setString(1, name)
+
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+
+        val player: PlacePlayerImpl = PlacePlayerImpl(
+            resultSet.getString("uuid"),
+            resultSet.getString("name")
+        )
+        player.setRanking(resultSet.getInt("ranking"))
+        player.setLastBlockRePlace(resultSet.getString("lastBlockRePlace").toLong())
+        player.setBlockToCount(resultSet.getInt("blocks"))
+        return player
     }
 
     override fun savePlacePlayer(placePlayer: PlacePlayer) {
-        TODO("Not yet implemented")
+        val deleteStatement = connection
+            .prepareStatement("DELETE FROM placePlayer WHERE uuid = ?")
+        deleteStatement.setString(1, placePlayer.getUUID().toString())
+        deleteStatement.executeUpdate()
+        createPlayerInDatabase(placePlayer)
     }
 
     override fun createPlayerInDatabase(placePlayer: PlacePlayer) {
-        TODO("Not yet implemented")
+        val preparedStatement: PreparedStatement = connection
+            .prepareStatement("INSERT INTO placePlayer(uuid, name, blocks, lastBlockRePlace, ranking) VALUES (?, ?, ?, ?, ?)")
+        preparedStatement.setString(1, placePlayer.getUUID().toString())
+        preparedStatement.setString(2, placePlayer.getName())
+        preparedStatement.setInt(3, placePlayer.getBlockCount())
+        preparedStatement.setLong(4, placePlayer.getLastBlockRePlace())
+        preparedStatement.setInt(5, placePlayer.getRanking())
+
+        preparedStatement.executeUpdate()
     }
 
     override fun getRanking(uuid: UUID): Int {
-        TODO("Not yet implemented")
+        return 0
     }
 
     override fun isBlockHistory(blockHistory: BlockHistory): Boolean {
-        TODO("Not yet implemented")
+        val preparedStatement = connection
+            .prepareStatement("SELECT * FROM placeBlockHistory WHERE location = ?")
+        preparedStatement.setString(1, blockHistory.getLocation())
+
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+        if (resultSet.next())
+            return true
+        return false
     }
 
     override fun addBlockHistory(blockHistory: BlockHistory) {
-        TODO("Not yet implemented")
+        val preparedStatement: PreparedStatement = connection
+            .prepareStatement("INSERT INTO placeBlockHistory(uuid, time, location) VALUES (?, ?, ?)")
+        preparedStatement.setString(1, blockHistory.getPlayer().toString())
+        preparedStatement.setString(2, blockHistory.getTime().toString())
+        preparedStatement.setString(3, blockHistory.getLocation())
+
+        preparedStatement.executeUpdate()
     }
 
     override fun getBlockHistory(location: String): BlockHistory {
-        TODO("Not yet implemented")
+        val preparedStatement = connection
+            .prepareStatement("SELECT * FROM placeBlockHistory WHERE location = ?")
+        preparedStatement.setString(1, location)
+
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+
+        return BlockHistoryImpl(
+            resultSet.getString("uuid"),
+            resultSet.getString("time").toLong(),
+            resultSet.getString("location")
+        )
     }
 
     override fun updateBlockHistory(blockHistory: BlockHistory) {
-        TODO("Not yet implemented")
+        deleteBlockHistory(blockHistory)
+        addBlockHistory(blockHistory)
     }
 
     override fun deleteBlockHistory(blockHistory: BlockHistory) {
-        TODO("Not yet implemented")
+        val deleteStatement = connection
+            .prepareStatement("DELETE FROM placeBlockHistory WHERE location = ?")
+        deleteStatement.setString(1, blockHistory.getLocation())
+        deleteStatement.executeUpdate()
     }
 
     override fun getBlockHistoryCount(): Int {
-        TODO("Not yet implemented")
+        val preparedStatement = connection
+            .prepareStatement("SELECT * FROM placeBlockHistory")
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+        return resultSet.fetchSize
     }
 }
